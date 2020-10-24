2c7ae16c370be315808b1b2a3f2d8b5c39f66a5f
MrMohammadY <m.yazdani000@gmail.com>
2020-10-24 17:20:18 +0330

    secends commit

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='pack',
-    version='0.1',
+    version='0.2',
     description='this first pack',
     url='#',
     author='MrMohammad',
     author_email='dsaidja',
     license='MIT',
     packages=['pack'],
+    install_requires=['requests'],
     zip_safe=False
 )
